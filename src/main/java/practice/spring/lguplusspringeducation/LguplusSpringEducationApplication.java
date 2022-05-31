d55bebe41dfda373a7a2d9fde3731e03eccaa2fe
kwangmin-park <96bgm@korea.ac.kr>
2022-05-31 09:20:46 +0900

    first commit

---
+++ src/main/java/practice/spring/lguplusspringeducation/LguplusSpringEducationApplication.java
@@ -0,0 +1,13 @@
+package practice.spring.lguplusspringeducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LguplusSpringEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LguplusSpringEducationApplication.class, args);
+	}
+
+}
